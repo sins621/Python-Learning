5ada1fff4d58706da0afd776d5246650ea159827
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-10-02 18:29:23 +0200

    update/finished-day-1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,4 @@
-# Write your code below this line 👇
-print("Hello world!")
+print("Hello! I Would be Happy to Generate a Band Name for You!")
+user_city = input("What is the name of the city that you grew up in?\n")
+user_pet = input("What is the name of your pet?\n")
+print("Your band name is " + user_city + " " +  user_pet)
