4f081f2c96698e02de2d2eb7309336c0a590cc07
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-10-04 18:25:42 +0200

    day 2

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,8 @@
-bmi = 84 / 1.65 ** 2
+print("Welcome to the tip calculator!")
+total = float(input("What was the total bill? $"))
+tip = total*(int(
+    input("How much tip would you like to give? 10, 12, or 15? "))
+             /100)
+people = int(input("How many people to split the bill? "))
+print(f"Each person should pay: $ {round((total+tip)/people,2)}")
+
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,8 @@
-len(12345)
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / height ** 2
+
+print(bmi)
