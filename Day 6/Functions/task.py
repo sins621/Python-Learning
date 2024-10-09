4d89e7c4759c616f24869b47a0cb4a483f1e9ef9
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-10-09 12:06:43 +0200

    update/day-6

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,30 @@
+# import os
+
+# clear = lambda: os.system("clear")
+# clear()
+
+# def printHello():
+#     print("Hello World!")
+
+# printHello()
+# front_is_clear()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    while wall_in_front()
+        turn_left()
+        move()
+        turn_right()
+    move()
+    turn_right()
+    move()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+         move()
