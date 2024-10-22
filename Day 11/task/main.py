6241859ad265708c0cb579d498644831a330ff84
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-10-22 10:40:19 +0200

    update/day-11-finished

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -11,95 +11,59 @@
 # Cards are not removed from the deck as they are drawn.
 # The computer is the dealer.
 
-import os
-import platform
 import art
 import random
 
-clear = lambda: (
-    os.system("cls") if platform.system() == "Windows" else os.system("clear")
-)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def sum_cards(hand):
+    total = 0
+    if sum(hand) == 21 and len(hand) == 2:
+        return 0
+    for i in hand:
+        if i == 11:
+            if sum(hand) > 21:
+                total += 1
+            else:
+                total += 11
+        else:
+            total += i
+    return total
 
-print(art.logo)
 
-playing = "y"
+CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 player_cards = []
 dealer_cards = []
+hit = "y"
 
-player_cards.append(random.choice(cards))
-player_cards.append(random.choice(cards))
-print(f"Your cards: {player_cards} Current Score: {sum(player_cards)}")
+print(art.logo)
+for i in range(2):
+    player_cards.append(random.choice(CARDS))
+    dealer_cards.append(random.choice(CARDS))
 
-dealer_cards.append(random.choice(cards))
-print(f"Computer's first card: {dealer_cards[0]}")
+player_total = sum_cards(player_cards)
+dealer_total = sum_cards(dealer_cards)
 
-hit = input("Type 'y' to get another card, 'n' to pass.\n")
+while hit == "y" and player_total < 22:
+    print(f"Your cards: {player_cards} Current Score: {player_total}")
+    print(f"Computer's first card: {dealer_cards[0]}")
+    hit = input("Type 'y' to get another card, 'n' to pass.\n")
+    if hit == "y":
+        player_cards.append(random.choice(CARDS))
+        player_total = sum_cards(player_cards)
 
-if hit == "n":
-    print(f"Your final hand: {player_cards} Current Score: {sum(player_cards)}")
+print(f"Your final hand: {player_cards} Current Score: {player_total}")
+while sum_cards(dealer_cards) < player_total or dealer_total < 17:
+    dealer_cards.append(random.choice(CARDS))
+    dealer_total = sum_cards(dealer_cards)
+print(f"Dealer's final Hand: {dealer_cards} Score: {dealer_total}")
 
-    while sum(dealer_cards) < sum(player_cards) or sum(dealer_cards) < 17:
-        dealer_cards.append(random.choice(cards))
-
-    print(f"Dealer's final Hand: {dealer_cards} Score: {sum(dealer_cards)}")
-
-if sum(player_cards) > 21:
+if player_total > 21:
     print("You went over, you lose")
-elif sum(dealer_cards) > 21:
+elif dealer_total > 21:
     print("Dealer went over, you win")
-elif sum(player_cards) == sum(dealer_cards):
+elif player_total == dealer_total:
     print("It's a draw")
-elif sum(player_cards) > sum(dealer_cards):
+elif player_total > dealer_total:
     print("You win")
 else:
     print("You lose")
-
-# lost = False
-
-# while playing == "y":
-#     player_cards.clear()
-#     player_cards.append(random.choice(cards))
-#     player_cards.append(random.choice(cards))
-#     print(f"Your cards: {player_cards} Current Score: {sum(player_cards)}")
-
-#     dealer_cards.clear()
-#     dealer_cards.append(random.choice(cards))
-#     print(f"Computer's first card: {dealer_cards[0]}")
-#     hit = input("Type 'y' to get another card, 'n' to pass.\n")
-
-#     while hit == "y":
-#         player_cards.append(random.choice(cards))
-#         print(f"Your cards: {player_cards} Current Score: {sum(player_cards)}")
-#         print(f"Computer's first card: {dealer_cards[0]}")
-
-#         hit = "n"
-
-#         if sum(player_cards) > 21:
-#             lost = True
-#         else:
-#             hit = input("Type 'y' to get another card, 'n' to pass.\n")
-
-#     while sum(dealer_cards) < 17 and lost == False:
-#         dealer_cards.append(random.choice(cards))
-
-#     print(
-#         f"Your final hand: {player_cards} Current Score: {sum(player_cards)}"
-#     )
-#     print(f"Dealer's final Hand: {dealer_cards} Score: {sum(dealer_cards)}")
-
-#     print("")
-
-#     if lost == True:
-#         print("You went over, you lose.")
-#     elif sum(dealer_cards) > 21:
-#         print("Dealer went over, you win.")
-#     elif sum(dealer_cards) > sum(player_cards):
-#         print("Dealer score is higher, you lose.")
-#     elif sum(dealer_cards) < sum(player_cards):
-#         print("Your score is higher, you win.")
-#     else:
-#         print("Draw")
-
-#     playing = input("Do you want to play a game of Blackjack? 'y' or 'n'\n")
