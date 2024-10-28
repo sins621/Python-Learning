f272cbb5c6fc9e2cdfb25910efa60b83d91a175b
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-10-28 14:41:12 +0200

    update/day-16-finished

--- Day 16/coffee_machine/main.py
+++ Day 16/coffee_machine/main.py
@@ -2,5 +2,21 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+is_on = True
+choice = ""
+CoffeeMaker = CoffeeMaker()
+Menu = Menu()
+MoneyMachine = MoneyMachine()
 
-
+while is_on == True:
+    choice = input(f"What would you like? ({Menu.get_items()})\n")
+    if choice == "report":
+        CoffeeMaker.report()
+        MoneyMachine.report()
+    elif choice == "off":
+        is_on = False
+    else:
+        order = Menu.find_drink(choice)
+        if CoffeeMaker.is_resource_sufficient(order):
+            if MoneyMachine.make_payment(order.cost):
+                CoffeeMaker.make_coffee(order)
