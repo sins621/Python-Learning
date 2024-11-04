f07bb001e828201b746b80614c409926d7777c11
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-11-04 14:46:36 +0200

    update/day-18

--- Day 18/main.py
+++ Day 18/main.py
@@ -4,14 +4,17 @@ from random import choice
 
 turtle = Turtle()
 screen = Screen()
-shapes = 10
+SHAPES = 10
+DISTANCE = 100
+MIN_SIDES = 3
 
 screen.bgcolor("black")
 # I'm the best
-for i in range(3,shapes + 1):
+for num_sides in range(MIN_SIDES,SHAPES + 1):
     turtle.color(choice(turtle_colors))
-    for j in range(i):
-        turtle.forward(100)
-        turtle.right(360/i)
+    angle = 360/num_sides
+    for j in range(num_sides):
+        turtle.forward(DISTANCE)
+        turtle.right(angle)
 
 screen.exitonclick()
