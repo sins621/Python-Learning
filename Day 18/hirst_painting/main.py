ee77607dc0d5bb9014b0eac3b28e6ee769533d67
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-11-05 10:53:15 +0200

    update/day-18

--- Day 18/hirst_painting/main.py
+++ Day 18/hirst_painting/main.py
@@ -1,10 +1,17 @@
 ###This code will not work in repl.it as there is no access to the colorgram package here.###
 ##We talk about this in the video tutorials##
 import colorgram
+import turtle as t
+
+t.colormode(255)
+turtle = t.Turtle()
+screen = t.Screen()
+screen.bgcolor("black")
+turtle.color("white")
 
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
 for color in colors:
     rgb_colors.append(color.rgb)
 
-print(colors)
+screen.exitonclick()
