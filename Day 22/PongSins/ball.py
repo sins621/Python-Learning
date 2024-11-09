5795d7f805fd0c4e50755ddafe43655c8eca427c
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-11-09 23:57:12 +0200

    update/day-22

--- Day 22/PongSins/ball.py
+++ Day 22/PongSins/ball.py
@@ -1,5 +1,30 @@
 from turtle import Turtle
 
+SPEED = 10
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
+        self.pu()
+        self.shape("circle")
+        self.color("white")
+        self.setheading(0)
+
+    def hor_collision(self, paddle_ycor, paddle_dis):
+        self.setheading((self.heading() - 180) * -1)
+
+        if self.heading() < 90 or self.heading() > 270:
+            if self.ycor() > paddle_ycor:
+                self.setheading(self.heading() + paddle_dis//4)
+            else:
+                self.setheading(self.heading() - paddle_dis//4)
+
+        else:
+            if self.ycor() > paddle_ycor:
+                self.setheading(self.heading() - paddle_dis//4)
+            else:
+                self.setheading(self.heading() + paddle_dis//4)
+
+    def ver_collision(self):
+        pass
+
--- Day 22/PongSins/main.py
+++ Day 22/PongSins/main.py
@@ -1,11 +1,11 @@
 from turtle import Screen, Turtle
 from layout import Layout
 from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
 layout = Layout(screen)
-
 p1_paddle = Paddle(layout.SCREEN_WIDTH, layout.SCREEN_HEIGHT,1)
 p2_paddle = Paddle(layout.SCREEN_WIDTH, layout.SCREEN_HEIGHT,2)
 screen.listen()
@@ -15,9 +15,32 @@ screen.onkey(p1_paddle.move_down, "s")
 screen.onkey(p2_paddle.move_up, "i")
 screen.onkey(p2_paddle.move_down, "k")
 
+ball = Ball()
+
+COLLISION_DISTANCE = 35
+
 # Game Loop
 is_running = True
 while is_running:
+    ball.forward(10)
+
+    p1_paddle.sety(ball.ycor())
+    p2_paddle.sety(ball.ycor())
+
+    if ball.distance(p1_paddle) < COLLISION_DISTANCE:
+        ball.hor_collision(p1_paddle.ycor(), ball.distance(p1_paddle))
+
+    if ball.distance(p2_paddle) < COLLISION_DISTANCE:
+        ball.hor_collision(p2_paddle.ycor(), ball.distance(p2_paddle))
+
+    if ball.ycor() > layout.SCREEN_HEIGHT//2:
+        ball.ver_collision()
+
+    if ball.ycor() < layout.SCREEN_HEIGHT//-2:
+        ball.ver_collision()
+
+    print(ball.pos())
+
     # Update Frame
     ######################################################
     time.sleep(0.016)
