d3b3c712e90af7e55147a3eaab7b68b2ba2ce089
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-11-14 20:42:33 +0200

    update/day-25

--- Day 24/MailMerge/main.py
+++ Day 24/MailMerge/main.py
@@ -8,10 +8,10 @@
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 names = []
 
-with open("Input/Names/invited_names.txt") as names:
+with open("./Input/Names/invited_names.txt") as names:
     names = names.readlines()
 
-with open("Input/Letters/starting_letter.txt") as letter:
+with open("./Input/Letters/starting_letter.txt") as letter:
     contents = letter.readlines()
     for name in names:
         new_string = ""
@@ -19,5 +19,5 @@ with open("Input/Letters/starting_letter.txt") as letter:
         for line in contents:
             new_line = line.replace("[name]", f"{new_name}")
             new_string += new_line
-        with open(f"Output/ReadyToSend/{new_name}.txt", mode="w") as new_letter:
+        with open(f"./Output/ReadyToSend/{new_name}.txt", mode="w") as new_letter:
             new_letter.write(new_string)
