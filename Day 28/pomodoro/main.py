782629ceb6aaf06ff4b2cf44791806db4318d4b9
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-11-19 21:48:02 +0200

    update/day-28

--- Day 28/pomodoro/main.py
+++ Day 28/pomodoro/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,10 +10,59 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(1*60)
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    count_min = count // 60
+    count_sec = count % 60
+
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=10, pady=5, bg=YELLOW)
+
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(row=0, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="./Day 28/pomodoro/tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
+canvas.grid(column=1, row=1)
+
+start_button = Button(
+    text="Start", bg=YELLOW, highlightthickness=0, command=start_timer
+)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+checkmarks = ["✓"]
+checkmark_label = Label(text=checkmarks, bg=YELLOW, fg=GREEN)
+checkmark_label.grid(row=3, column=1)
+
+window.mainloop()
