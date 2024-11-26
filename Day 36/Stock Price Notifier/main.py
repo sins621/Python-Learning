0921bb4e5675f82a2bfbf308ec34d5fd1cb32cf7
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-11-26 05:47:06 +0200

    update/day-36

--- Day 36/Stock Price Notifier/main.py
+++ Day 36/Stock Price Notifier/main.py
@@ -1,3 +1,6 @@
+import os
+from twilio.rest import Client
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -5,13 +8,21 @@ COMPANY_NAME = "Tesla Inc"
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 ## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 ## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# Send a seperate message with the percentage change and each article's title and description to your phone number.
+TWILIO_SID = os.environ.get("TWILIO_SID")
+TWILIO_AUTH = os.environ.get("TWILIO_AUTH")
 
+def send_sms(body_text):
+    client = Client(TWILIO_SID, TWILIO_AUTH)
+    message = client.messages.create(
+        from_="+17753682906", body=body_text, to="+27765261782"
+    )
+    print(message.status)
 
-#Optional: Format the SMS message like this: 
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -21,4 +32,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
