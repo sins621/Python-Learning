a71f215b97a24a63f413894e80b9c76cf092e8c8
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-12-03 22:41:01 +0200

    update/stuff

--- Day 39/Flight Notifier/main.py
+++ Day 39/Flight Notifier/main.py
@@ -1,1 +1,8 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, 
+# NotificationManager classes to achieve the program requirements.
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEETY_KEY = os.getenv('SHEETY')
