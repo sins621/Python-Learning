649a8089fd93c21986eb7aaab03aba20ca4d2433
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-12-05 16:03:06 +0200

    update/day-39

--- Day 39/Flight Notifier/data_manager.py
+++ Day 39/Flight Notifier/data_manager.py
@@ -12,7 +12,7 @@ PATH = "./Day 39/Flight Notifier"
 get_endpoint = (
     "https://api.sheety.co/bc2d347620e0173a47d425cfc87639fd/flightDeals/prices"
 )
-post_endpoint = (
+put_endpoint = (
     "https://api.sheety.co/bc2d347620e0173a47d425cfc87639fd/flightDeals/prices"
 )
 
@@ -21,24 +21,46 @@ sheety_header = {"Authorization": f"Bearer {SHEETY_AUTH}"}
 
 class DataManager:
     def __init__(self):
-        #################################################################
-        #                        Sheety Get                             #
-        #################################################################
+        pass
 
-        # try:
-        #     self.sheety_get = requests.get(url=get_endpoint, headers=sheety_header)
-        #     self.sheety_get.raise_for_status()
-        # except requests.exceptions.HTTPError as errh:
-        #     print(f"HTTP Error: {errh.args[0]}")
+    def get_sheet_data(self):
+        try:
+            self.sheety_get = requests.get(url=get_endpoint, headers=sheety_header)
+            self.sheety_get.raise_for_status()
+            print(f"Response: {self.sheety_get}")
+            return self.sheety_get.json()
+        except requests.exceptions.HTTPError as errh:
+            print(f"HTTP Error: {errh.args[0]}")
+            return None
 
-        # print(f"Response: {self.sheety_get}")
-        # print(json.dumps(self.sheety_get.json(), indent=2))
-
-        #################################################################
-        #                       Import Test Json                        #
-        #################################################################
+    def get_test_sheet_data(self):
+        test_data = None
         try:
             with open(f"{PATH}/sheety_get.json", "r") as test_file:
-                self.test_data = json.load(test_file)
+                test_data = json.load(test_file)
+            return test_data
         except FileNotFoundError as errf:
             print(f"File Not Found, Error: {errf}")
+            return None
+
+    def get_test_updated_sheet_data(self):
+        test_data = None
+        try:
+            with open(f"{PATH}/sheety_updated.json", "r") as test_file:
+                test_data = json.load(test_file)
+            return test_data
+        except FileNotFoundError as errf:
+            print(f"File Not Found, Error: {errf}")
+            return None
+    
+    def update_sheet_data(self, city, id):
+        body = {
+            "price": city
+        }
+        try:
+            response = requests.put(url=f"{put_endpoint}/{id}", json=body, headers=sheety_header)
+            print(f"Response is: {response}")
+            response.raise_for_status()
+            print("Put Request Successful")
+        except requests.exceptions.HTTPError as errh:
+            print(f"HTTP Error: {errh.args[0]}")
--- Day 39/Flight Notifier/flight_data.py
+++ Day 39/Flight Notifier/flight_data.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import json
 import os
 
@@ -6,37 +8,91 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+current_date = datetime.now()
+current_date_only = current_date.date()
+
+date_in_6_months = current_date_only + relativedelta(months=6)
+
 AMADEUS_KEY = os.getenv("AMADEUS_KEY")
 AMADEUS_SECRET = os.getenv("AMADEUS_SECRET")
 AMADEUS_TOKEN = os.getenv("AMADEUS_TOKEN")
 PATH = "./Day 39/Flight Notifier"
 
 amadeus_city_get = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+amadeus_flight_offer_get = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 
 
 class FlightData:
-    def __init__(self, **kw):
-        self.params = {"keyword": kw.get("city"), "max": 1}
-        self.headers = {"Authorization": f"Bearer {AMADEUS_TOKEN}"}
-        #################################################################
-        #                          City Get                             #
-        #################################################################
-        # try:
-        #     self.city_get = requests.get(
-        #         url=amadeus_city_get, headers=self.headers, params=self.params
-        #     )
-        #     self.city_get.raise_for_status()
-        # except requests.exceptions.HTTPError as errh:
-        #     print(f"HTTP Error: {errh.args[0]}")
-        # else:
-        #     print(self.city_get)
-        #     print(json.dumps(self.city_get.json(), indent=2))
+    def __init__(self):
+        self.token = self.get_access_token()
 
-        #################################################################
-        #                       Import Test Json                        #
-        #################################################################
+    def get_access_token(self):
+        url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        body = {
+            "grant_type": "client_credentials",
+            "client_id": AMADEUS_KEY,
+            "client_secret": AMADEUS_SECRET,
+        }
+        try:
+            response = requests.post(url, headers=headers, data=body)
+            response.raise_for_status()
+            return response.json()["access_token"]
+        except requests.exceptions.RequestException as err:
+            print(f"Error fetching access token: {err}")
+            return None
+
+    def get_city_info(self, city):
+        if not self.token:
+            print("No valid token available.")
+            return None
+
+        body = {"keyword": city, "max": 1}
+        headers = {"Authorization": f"Bearer {self.token}"}
+        try:
+            city_get = requests.get(url=amadeus_city_get, headers=headers, params=body)
+            city_get.raise_for_status()
+            city_data = city_get.json()["data"][0]
+            return city_data
+        except requests.exceptions.HTTPError as errh:
+            print(f"HTTP Error: {errh}")
+        except requests.exceptions.RequestException as err:
+            print(f"Request Error: {err}")
+
+    def get_test_city_info(self):
         try:
             with open(f"{PATH}/city_get.json", "r") as test_file:
-                self.test_data = json.load(test_file)["data"]
+                test_data = json.load(test_file)
+            return test_data
         except FileNotFoundError as errf:
             print(f"File Not Found, Error: {errf}")
+            return None
+
+    def get_flight_deal(self, origin_iata, destination_iata, max_price):
+        if not self.token:
+            print("No valid token available.")
+            return None
+
+        body = {
+            "originLocationCode": origin_iata,
+            "destinationLocationCode": destination_iata,
+            "departureDate": current_date_only,
+            "returnDate": date_in_6_months,
+            "adults": 1,
+            "currencyCode": "EUR",
+            "maxPrice": max_price,
+        }
+        headers = {"Authorization": f"Bearer {self.token}"}
+        try:
+            response = requests.get(
+                url=amadeus_flight_offer_get, headers=headers, params=body
+            )
+            response.raise_for_status()
+            flight_deal_data = response.json()
+            return flight_deal_data
+        except requests.exceptions.HTTPError as errh:
+            print(f"HTTP Error: {errh}")
+            return None
+        except requests.exceptions.RequestException as err:
+            print(f"Request Error: {err}")
+            return None
