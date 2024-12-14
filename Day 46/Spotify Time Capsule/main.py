b90bd8cccd11906bd4673f0f6f59f2c2861808c2
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-12-14 09:48:22 +0200

    update/day-46-finished

--- Day 46/Spotify Time Capsule/main.py
+++ Day 46/Spotify Time Capsule/main.py
@@ -17,12 +17,12 @@ spotify_auth = spotipy.oauth2.SpotifyOAuth(
     redirect_uri="http://example.com",
 )
 
-spotify_token = spotify_auth.get_access_token()["access_token"]
+spotify_token = spotify_auth.get_access_token(as_dict=False)
 
 music_date = input(
     "Which year do you want to travel to? Type the date in this format: YYYY-MM-DD\n"
 )
-music_date = "2010-02-18"
+
 
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"
@@ -38,16 +38,25 @@ soup = BeautifulSoup(website_html, "html.parser")
 song_names_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_spans]
 
-
 sp = spotipy.Spotify(auth=spotify_token)
-user = sp.current_user()["id"]
+
+user = sp.current_user()["id"]  # pyright: ignore
+
+playlist_id = sp.user_playlist_create(  # pyright: ignore
+    user=user,
+    name=f"{music_date} Billboard Top 100",
+    public=False,
+    description=f"Playlist with the top 100 tracks from {music_date}",
+)["id"]
 
 music_year = music_date.split("-")[0]
+track_urls = []
 
 for song in song_names:
-    song_url = sp.search(
-        q=f"track:{song} year:{music_year}", type="track", limit=1
-    )
-    track_url = song_url["tracks"]["items"][0]["external_urls"]["spotify"]
-    # print(json.dumps(song_url, indent=2))
-    print(track_url)
+    song_url = sp.search(q=f"track:{song} year:{music_year}", type="track", limit=1)
+    track_url = song_url["tracks"]["items"][0]["external_urls"][  # pyright: ignore
+        "spotify"
+    ]
+    track_urls.append(track_url)
+
+sp.user_playlist_add_tracks(user=user, playlist_id=playlist_id, tracks=track_urls)
