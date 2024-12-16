54142a66148ed29d98e4da3b95a41bc2e7253b0e
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-12-16 17:18:52 +0200

    update/day-48-finished

--- Day 48/Selenium/Cookie Clicker Exercise/main.py
+++ Day 48/Selenium/Cookie Clicker Exercise/main.py
@@ -1,8 +1,7 @@
-from datetime import datetime, timedelta
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -13,8 +12,22 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, value="cookie")
 count = 0
 
+purchase_timer = time.time() + 5
+
+
+def attempt_purchase(store_buttons, money):
+    for button in reversed(store_buttons):
+        if button.text != "":
+            button_cost = int(button.text.split("-")[1].strip().replace(",", ""))
+            if money > button_cost:
+                button.click()
+                return
+
 
 while True:
     cookie.click()
-    count += 1
-    print(count)
+    if time.time() > purchase_timer:
+        store_buttons = driver.find_elements(By.CSS_SELECTOR, value="div#store b")
+        money = int(driver.find_element(By.ID, value="money").text.replace(",", ""))
+        attempt_purchase(store_buttons, money)
+        purchase_timer = time.time() + 10
