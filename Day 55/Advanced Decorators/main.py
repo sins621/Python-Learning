44b86597672566473402e5e9fec0a68e035e0e7a
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-12-24 20:05:18 +0200

    update/day-55-finished

---
+++ Day 55/Advanced Decorators/main.py
@@ -0,0 +1,21 @@
+# TODO: Create the logging_decorator() function 👇
+
+
+def logging_decorator(function):
+    def wrapper(*args):
+        print(f"You called {function.__name__}{args}")
+        print(f"It returned: {function(*args)}")
+        return function(*args)
+
+    return wrapper
+
+
+# TODO: Use the decorator 👇
+
+
+@logging_decorator
+def a_function(*args):
+    return sum(args)
+
+
+a_function(1, 2, 3)
--- Day 55/URL Parsing/main.py
+++ Day 55/URL Parsing/main.py
@@ -3,18 +3,47 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper_function():
+        return f"<b>{function()}</b>"
+
+    return wrapper_function
+
+
+def make_emphasis(function):
+    def wrapper_function():
+        return f"<em>{function()}</em>"
+
+    return wrapper_function
+
+
+def make_underlined(function):
+    def wrapper_function():
+        return f"<u>{function()}</u>"
+
+    return wrapper_function
+
+
 @app.route("/")
 def hello_world():
-    return "<h1 style='text-align: center'>Cool Heading</h1>" \
+    return (
+        "<h1 style='text-align: center'>Cool Heading</h1>"
         "<img src='https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjdiZ24waDhxdmZndjc3czFwczY2c255eDI2MXRrMGd6YWVsOWFhMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YHYmMLkOmqoo/giphy.gif' style='margin-left: 25%'>"
+    )
+
 
 @app.route("/bye")
+@make_bold
+@make_underlined
+@make_emphasis
 def bye():
     return "Bye"
 
+
 @app.route("/<user>")
 def greet(user):
-    return f"Hello {user + 5}"
+    return f"Hello {user}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
