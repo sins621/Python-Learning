dfd646e317e042c001ba8051baa49c8ac80d4f63
Bradly Carpenter <bradlycarpenterza@gmail.com>
2025-01-08 12:28:36 +0200

    update/day-63

--- Day 63/Library Project/main.py
+++ Day 63/Library Project/main.py
@@ -1,12 +1,47 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Float, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+# TODO: 1. Display DB @ home
+# TODO: 2. Add New Books @ add
+# TODO: 3. Add <a>Change Rating</a> To Each Element
+# TODO: 4. Add <a>Delete</a> To Each Element
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.sqlite3"
+
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __init__(self, title, author, rating):
+        self.title = title
+        self.author = author
+        self.rating = rating
+
+
 all_books = []
 
+# with app.app_context():
+#     db.create_all()
+#     new_book = Book(title="Harry Potter", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def home():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+    if request.method == "POST":
+        pass
     return render_template("index.html", books=all_books)
 
 
@@ -14,12 +49,13 @@ def home():
 def add():
     if request.method == "POST":
         data = request.form
-        book_data = {
-            "title": data["book_name"],
-            "author": data["book_author"],
-            "rating": data["book_rating"],
-        }
-        all_books.append(book_data)
+        book_data = Book(
+            title=data["book_name"],
+            author=data["book_author"],
+            rating=data["book_rating"],
+        )
+        db.session.add(book_data)
+        db.session.commit()
     return render_template("add.html")
 
 
