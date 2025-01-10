f613c05e71648ebabca013f0f2b350c81a44caab
sins621 <bradlycarpenterza@gmail.com>
2025-01-10 12:17:34 +0200

    update/day-64-db-creation-and-display

--- Day 64/Favourite Movies Project/main.py
+++ Day 64/Favourite Movies Project/main.py
@@ -8,33 +8,49 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap5(app)
 
-# CREATE DB
+
+class Base(DeclarativeBase):
+    pass
 
 
-# CREATE TABLE
+db = SQLAlchemy(model_class=Base)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[String] = mapped_column(String(250), unique=True)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[String] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[String] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[String] = mapped_column(String(250), nullable=False)
+
+    def __init__(self, **kw):
+        self.title = kw.get("title", None)
+        self.year = kw.get("year", None)
+        self.description = kw.get("description", None)
+        self.rating = kw.get("rating", None)
+        self.ranking = kw.get("ranking", None)
+        self.review = kw.get("review", None)
+        self.img_url = kw.get("img_url", None)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies_query = db.session.execute(
+        db.select(Movie).order_by(Movie.ranking.desc())
+    ).scalars()
+    return render_template("index.html", movies=movies_query)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
