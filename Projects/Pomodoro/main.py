9155b232b859f9dfc02c024ddd073a8737524b53
sins621 <bradlycarpenterza@gmail.com>
2025-01-19 09:43:34 +0200

    add/textual-tests

--- Projects/Pomodoro/main.py
+++ Projects/Pomodoro/main.py
@@ -1,81 +1,25 @@
-import os
-import platform
-import time
-from datetime import date
+from textual.app import App, ComposeResult
+from textual.containers import Container, Horizontal
+from textual.widgets import Button, Footer, Header, Static
 
-clear = lambda: (
-    os.system("cls") if platform.system() == "Windows" else os.system("clear")
-)
-bar_width = lambda: os.get_terminal_size().columns - 2
-append_minute = lambda i: "s" if i > 119 else ""
-append_second = lambda i: "s" if i > 1 else ""
+QUESTION = "Do you want to learn about Textual CSS?"
 
 
-def timer(seconds):
-    clear()
-    for i in reversed(range(seconds)):
-        if i >= 60:
-            print(f"{i//60} Minute{append_minute(i)} remaining.")
-        elif i > 0:
-            print(f"{i} Second{append_second(i)} remaining.")
-
-        progress_percent = (seconds - i) / seconds
-        plus_chars = "+" * int(bar_width() * progress_percent)
-        minus_chars = "-" * int(bar_width() - len(plus_chars))
-        print(f"[{plus_chars}{minus_chars}]")
-        time.sleep(1)
-        print("\x1B[1A\x1B[0K\x1B[1A\x1B[0K", end="")
-#         if seconds % 120 == 0:
-#             clear()
+class ExampleApp(App):
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Container(
+            Static(QUESTION, classes="question"),
+            Horizontal(
+                Button("Yes", variant="success"),
+                Button("No", variant="error"),
+                classes="buttons",
+            ),
+            id="dialog",
+        )
 
 
-# def append_to_logfile(string, filename):
-#     logfile = open(f"{filename}.txt", "a")
-#     logfile.write(string)
-#     logfile.close()
-
-# def create_logfile():
-#     f = open(f"{date.today()}.txt", "x")
-
-
-print(
-    """
---------------------------------------------------------\n
--              Welcome to the Pomodoro Timer           -\n
---------------------------------------------------------\n
-    """
-)
-
-
-minutes = int(
-    input("How long would you like your working splits to be? (Minutes): \n")
-)
-
-split_seconds = minutes * 60
-
-minutes = int(input("How long would you like your breaks to be? (Minutes): \n"))
-break_seconds = minutes * 60
-
-working = "y"
-
-while working == "y":
-    print("Working..")
-    timer(split_seconds)
-
-    print("Work split is over")
-    time.sleep(1)
-    clear()
-
-    working = input(
-        "Are you ready to start your break? ('y' = yes, 'q' = quit)\n"
-    )
-    clear()
-
-    print("Break time!")
-    timer(break_seconds)
-
-    working = input(
-        "Would you like to continue working? ('y' = Yes, 'n' = No)\n"
-    )
-
-print("See you later")
+if __name__ == "__main__":
+    app = ExampleApp()
+    app.run()
