fcf41f1ba98c07d98bacbbe8e798a66a6b0718c5
sins621 <bradlycarpenterza@gmail.com>
2025-01-25 13:58:43 +0200

    update/tui-countdown-working

--- Projects/Pomodoro/main.py
+++ Projects/Pomodoro/main.py
@@ -1,5 +1,3 @@
-from time import monotonic
-
 from textual.app import App, ComposeResult
 from textual.containers import Container
 from textual.reactive import reactive
@@ -9,38 +7,25 @@ from textual.widgets import Button, Digits
 class TimeDisplay(Digits):
     """A Widget to display elapsed time."""
 
-    start_time = reactive(monotonic)
-    time = reactive(0.0)
-    total = reactive(0.0)
+    time = reactive(300)
 
-    def on_mount(self) -> None:
-        """Event handler called when widget is added to the app."""
-        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
+    def on_mount(self, time):
+        self.update_timer = self.set_interval(1, self.update_time, pause=True)
+        self.update(f"{time}")
 
-    def update_time(self) -> None:
-        """Method to update the time to the current time."""
-        self.time = self.total + (monotonic() - self.start_time)
+    def update_time(self):
+        self.time -= 1
 
-    def watch_time(self, time: float) -> None:
-        """Called when the time attribute changes."""
+    def watch_time(self, time):
         minutes, seconds = divmod(time, 60)
-        self.update(f"{minutes:02.0f}:{seconds:02.0f}")
+        self.update(f"{minutes:02,.0f}:{seconds:02.0f}")
 
-    def start(self) -> None:
-        """Method to start (or resume) time updating."""
-        self.start_time = monotonic()
+    def start(self):
         self.update_timer.resume()
 
-    def stop(self) -> None:
-        """Method to stop the time display updating."""
+    def stop(self):
         self.update_timer.pause()
-        self.total += monotonic() - self.start_time
-        self.time = self.total
-
-    # def reset(self) -> None:
-    #     """Method to reset the time display to zero."""
-    #     self.total = 0
-    #     self.time = 0
+        self.time = 40
 
 
 class Stopwatch(Container):
@@ -56,12 +41,9 @@ class Stopwatch(Container):
         elif button_id == "stop":
             time_display.stop()
             self.remove_class("started")
-        # elif button_id == "reset":
-        #     time_display.reset()
 
     def compose(self) -> ComposeResult:
         """Create child widgets of a stopwatch."""
-        # yield Button("Reset", id="reset")
         yield TimeDisplay()
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
