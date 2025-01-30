6c1eedd842cf5694d5edb74e0c1ccd301ed7854c
sins621 <bradlycarpenterza@gmail.com>
2025-01-30 11:58:05 +0200

    update/added-ids

--- Projects/Pomodoro/main.py
+++ Projects/Pomodoro/main.py
@@ -4,7 +4,7 @@ from textual.widgets import Button, Digits, TabbedContent, TabPane
 
 
 class TimeDisplay(Digits):
-    time = reactive(40 * 60)
+    time = reactive(5)
 
     def on_mount(self, time):
         self.update_timer = self.set_interval(1, self.update_time, pause=True)
@@ -16,10 +16,11 @@ class TimeDisplay(Digits):
     def watch_time(self, time):
         minutes, seconds = divmod(time, 60)
         self.update(f"{minutes:02,.0f}:{seconds:02.0f}")
+        print(time)
+        print(self.time)
 
     def start(self):
         self.update_timer.resume()
-        # update
 
     def stop(self):
         self.update_timer.pause()
@@ -27,8 +28,10 @@ class TimeDisplay(Digits):
 
 
 class Stopwatch(TabPane):
-    def __init__(self, title):
+    def __init__(self, title, id):
         super().__init__(title=title)
+        self.id = id
+        self.time = reactive(self.query_one(TimeDisplay).time)
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         button_id = event.button.id
@@ -45,19 +48,33 @@ class Stopwatch(TabPane):
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
 
+    def action_show_tab(self, tab: str) -> None:
+        """Switch to a new tab."""
+        self.get_child_by_type(TabbedContent).active = tab
+
 
 class Pomodoro(App):
     CSS_PATH = "stopwatch03.tcss"
+
+    def __init__(self):
+        self.time = reactive(self.query_one(Stopwatch).time)
 
     def on_mount(self) -> None:
         self.theme = "gruvbox"
 
     def compose(self) -> ComposeResult:
-        with TabbedContent():
-            yield Stopwatch("Focus")
-            yield Stopwatch("Short Break")
-            yield Stopwatch("Long Break")
+        with TabbedContent(initial="focus"):
+            yield Stopwatch("Focus", id="focus")
+            yield Stopwatch("Short Break", id="short_break")
+            yield Stopwatch("Long Break", id="long_break")
 
+    def watch_time(self, time):
+        if time == 0:
+            self.query_one(TabbedContent).active = "short_break"
+
+    # Switch Tab Function Here:
+        # if focus tab timer == 0:
+            # self.query_one(TabbedContent).active = "short_break"
 
 if __name__ == "__main__":
     app = Pomodoro()
